7c3851c576d46df78d08cec8a625b5a721d92324
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-05-05 18:59:31 -0700

    Add fibonacci(n) for n∈[2,10)

--- spec/fibonacciSpec.js
+++ spec/fibonacciSpec.js
@@ -7,4 +7,21 @@ describe('fibonacci', function() {
    it('should return 1 when given 1 as an argument', function() {
       expect(fibonacci(1)).to.equal(1);
    });
+
+   var fn_2 = 0
+   var fn_1 = 1;
+   for (var i = 2; i < 10; i++) {
+     var fn = fn_1 + fn_2;
+     it('should return ' + fn + ' when given ' + i + ' as an argument',
+       function() {
+         var _fn = fn
+         var _i = i;
+         return function() {
+           expect(fibonacci(_i)).to.equal(_fn);
+         };
+       }()
+     );
+     fn_2 = fn_1;
+     fn_1 = fn;
+   }
 });
