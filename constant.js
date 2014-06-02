cac5e212f4b47c328842bcd63334639895ff25dc
Drew Cuthbertson <syeoryn@gmail.com>
2014-06-02 10:05:33 -0700

    Adds undefinedError

--- constant.js
+++ constant.js
@@ -210,8 +210,12 @@ var maybeError = function() {
 };
 
 var rangeError = function(){
-  throw new rangeError('Maximum call stack size exceeded');
+  throw new RangeError('Maximum call stack size exceeded');
 };
+
+var undefinedError = function(){
+  throw new TypeError('undefined is not a function');
+}
 
 function fibonacci(n) {
   return [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269, 2178309, 3524578][n];
@@ -256,3 +260,5 @@ var usefulRandom = function(upper, lower, makeInteger){
   }
   return random
 }
+
+
